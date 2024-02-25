5ecf2bdcd54447da876d39220fc97852d4fc749a
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2024-02-25 15:56:51 +0530

    Added Selection Sort

---
+++ src/main/java/com/bhaskor/IntroToAlgoClrs/chapter2/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.bhaskor.IntroToAlgoClrs.chapter2;
+
+/**
+ * Exercise 2.2-3: Implementing selection sort
+ * 
+ * @author bhaskor
+ */
+public class SelectionSort {
+
+	public int[] sortArray(int[] inputArray) {
+		
+		for(int i=0;i<inputArray.length;i++) {
+			int smallest = Integer.MAX_VALUE;
+			int position = -1;
+			for(int j=i;j<inputArray.length;j++) {
+				if(inputArray[j] < smallest) {
+					smallest = inputArray[j];
+					position = j;
+				}
+			}
+			int temp = inputArray[i];
+			inputArray[i] = smallest;
+			inputArray[position] = temp;
+		}
+		
+		return inputArray;
+	}
+}
