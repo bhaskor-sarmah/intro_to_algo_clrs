1322b61cd38d2a91dd5f5a808b7f544c2b53b92d
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2024-03-23 16:38:45 +0530

    Added Dynamic Array class with unit tests

---
+++ src/main/java/com/bhaskor/moshHamedaniDsa/array/DynamicArray.java
@@ -0,0 +1,78 @@
+package com.bhaskor.moshHamedaniDsa.array;
+
+/**
+ * <p>
+ * Exercise: Building an Dynamic Array Array should automatically grow when
+ * adding items and it should automatically shrink when removing items
+ * 
+ * <p>
+ * Methods to be created <code>insert() removeAt() size() indexOf() getItems()</code>
+ * 
+ * @author bhaskor
+ */
+public class DynamicArray {
+
+	private int arraySize;
+	private int[] internalArray;
+	private int noOfElement = 0;
+
+	DynamicArray(final int initialSize) {
+		arraySize = initialSize;
+		internalArray = new int[arraySize];
+	}
+
+	public void insert(final int element) {
+		if (noOfElement >= arraySize) {
+			resizeArray();
+		}
+		internalArray[noOfElement++] = element;
+	}
+
+	public void removeAt(int index) {
+		if (index < 0 || index >= noOfElement) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		for (int i = index; i < noOfElement - 1; i++) {
+			internalArray[i] = internalArray[i + 1];
+		}
+		noOfElement--;
+	}
+
+	public int indexOf(int element) {
+		for (int i = 0; i < noOfElement; i++) {
+			if (internalArray[i] == element)
+				return i + 1;
+		}
+		return -1;
+	}
+
+	public int size() {
+		return noOfElement;
+	}
+	
+	public int[] getItems() {
+		final int[] resultArray = new int[noOfElement+1];
+		
+		copyArrayDataIntoNewArray(resultArray);
+		
+		return resultArray;
+	}
+
+	private void resizeArray() {
+		// Growing the Array size by 50% of original size
+		arraySize *= 1.5;
+		int[] newArray = new int[(int) (arraySize)];
+
+		copyArrayDataIntoNewArray(newArray);
+
+		internalArray = newArray;
+	}
+
+	private void copyArrayDataIntoNewArray(int[] newArray) {
+		for (int i = 0; i < noOfElement; i++) {
+			newArray[i] = internalArray[i];
+		}
+	}
+
+}
---
+++ src/test/groovy/com/bhaskor/moshHamedaniDsa/array/DynamicArraySpec.groovy
@@ -0,0 +1,135 @@
+package com.bhaskor.moshHamedaniDsa.array
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+/**
+ * Unit test for {@link DynamicArray}
+ * 
+ * @author bhaskor
+ */
+class DynamicArraySpec  extends Specification{
+	
+	@Subject
+	DynamicArray dynamicArray;
+	
+	def dynamicArrayInitialSize = 3
+	
+	def setup() {
+		dynamicArray = new DynamicArray(dynamicArrayInitialSize)
+	}
+	
+	def "Insert two elements in the array should insert successfully"() {
+		given:
+		
+		when:
+		dynamicArray.insert(5)
+		dynamicArray.insert(3)		
+		
+		then:
+		dynamicArray.size() == 2
+		dynamicArray.getItems()[0] == 5
+		dynamicArray.getItems()[1] == 3
+	}
+	
+	def "Insert multiple elements in the array which will resize the array"() {
+		given:
+		
+		when:
+		dynamicArray.insert(15)
+		dynamicArray.insert(35)
+		dynamicArray.insert(46)
+		dynamicArray.insert(33)
+		dynamicArray.insert(95)
+		dynamicArray.insert(20)
+		dynamicArray.insert(54)
+		dynamicArray.insert(78)
+		dynamicArray.insert(16)
+		
+		then:
+		dynamicArray.size() == 9
+		dynamicArray.getItems()[0] == 15
+		dynamicArray.getItems()[1] == 35
+		dynamicArray.getItems()[2] == 46
+		dynamicArray.getItems()[3] == 33
+		dynamicArray.getItems()[4] == 95
+		dynamicArray.getItems()[5] == 20
+		dynamicArray.getItems()[6] == 54
+		dynamicArray.getItems()[7] == 78
+		dynamicArray.getItems()[8] == 16
+	}
+	
+	def "Delete one elements from the array using index should delete the element"() {
+		given:
+		dynamicArray.insert(5)
+		dynamicArray.insert(3)
+		dynamicArray.insert(10)
+		
+		when:
+		dynamicArray.removeAt(1)
+		
+		then:
+		dynamicArray.size() == 2
+		dynamicArray.getItems()[0] == 5
+		dynamicArray.getItems()[1] == 10
+	}
+	
+	def "Delete elements from the array using index when index doesn't exist shout throw IndexOutOfBoundsException"() {
+		given:
+		dynamicArray.insert(5)
+		dynamicArray.insert(3)
+		dynamicArray.insert(10)
+		
+		when:
+		dynamicArray.removeAt(7)
+		
+		then:
+		thrown IndexOutOfBoundsException
+		
+		when:
+		dynamicArray.removeAt(-1)
+		
+		then:
+		thrown IndexOutOfBoundsException
+	}
+	
+	
+	def "Calling indexOf when data available in the array should return the index"() {
+		given:
+		dynamicArray.insert(75)
+		dynamicArray.insert(23)
+		dynamicArray.insert(10)
+		dynamicArray.insert(33)
+		dynamicArray.insert(95)
+		dynamicArray.insert(20)
+		dynamicArray.insert(54)
+		dynamicArray.insert(78)
+		dynamicArray.insert(16)
+		
+		when:
+		def indexOfElement = dynamicArray.indexOf(54)
+		
+		then:
+		indexOfElement == 7
+	}
+	
+	def "Calling indexOf when data is not available in the array should return -1"() {
+		given:
+		dynamicArray.insert(75)
+		dynamicArray.insert(23)
+		dynamicArray.insert(10)
+		dynamicArray.insert(33)
+		dynamicArray.insert(95)
+		dynamicArray.insert(20)
+		dynamicArray.insert(54)
+		dynamicArray.insert(78)
+		dynamicArray.insert(16)
+		
+		when:
+		def indexOfElement = dynamicArray.indexOf(88)
+		
+		then:
+		indexOfElement == -1
+	}
+	
+}
